3289661b17f7f14c04373a12eee8bd38b4dadf41
elishagretton <50020933+elishagretton@users.noreply.github.com>
2021-02-09 10:45:21 +0000

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -5,146 +5,149 @@
 #include <algorithm>
 using namespace std;
 
-//this is just for the simple example
-
-//print list of strings
-void print(std::list<std::string> const &list)
-{
-    for (auto const& i: list) {
-        std::cout << i << "\n";
-    }
-}
-
-//print list of integers
-void print1(std::list<int> const &list)
-{
-    for (auto const& i: list) {
-        std::cout << i << ",";
-    }
-}
-
-//function to find combinations. e.g. if you have N things and want to organise them in groups of K
-//e.g 5 things organised in 3 slots
-//comb(5,3) = 0 1 2, 0 1 3 etc
-void comb(int N, int K)
-{
-    std::string bitmask(K, 1); // K leading 1's
-    bitmask.resize(N, 0); // N-K trailing 0's
-
-    // print integers and permute bitmask
-    do {
-        for (int i = 0; i < N; ++i) // [0..N-1] integers
-        {
-            if (bitmask[i]) std::cout << " " << i;
-        }
-        std::cout << std::endl;
-    } while (std::prev_permutation(bitmask.begin(), bitmask.end()));
-}
-//void formula(int t_2,int t_3,int t_4, int num_of_pizzas){
-//    int a,b,c;
-//    std::list<int> values;
-//    num_of_pizzas = 2*a + 3*b + 4*c;
-//    for (int i = 0; t_2; i++){
-//        for (int j = 0;t_3;j++){
-//            c=(2*i+3*j-num_of_pizzas)/4;
-//            values.push_back(i);values.push_back(j);values.push_back(c);
-//        }
-//    }
-//}
-
-int main()
-{
-    //setting variables
-    int num_of_pizzas=5;
-    int t_2 = 1; int t_3 = 2; int t_4 = 1;
-
-    //setting pizzas + ingredients
-    //later I will work on inputting the file and getting the format correct for vars
-    //for now, just set them
-    std::list<string> p1 = {"onion","pepper","olive"};
-    std::list<string> p2 = {"mushroom","tomato","basil"};
-    std::list<string> p3 = {"chicken","mushroom","pepper"};
-    std::list<string> p4 = {"tomato","mushroom","basil"};
-    std::list<string> p5 = {"chicken","basil"};
-
-    //add each pizza's ingredients to one list, p1
-    p1.merge(p2); p1.merge(p3);p1.merge(p4);p1.merge(p5);
-
-    //get rid of repeats so there are no repeated ingredients
-    p1.sort();
-    p1.unique();
-    std::list<string> all_pizza_ingredients = p1;
-    print(all_pizza_ingredients);
-
-    //size of new list is num of diff ingredients
-    int num_of_ingredients = all_pizza_ingredients.size();
-    std::cout<<"size:"<<num_of_ingredients<<"\n";
-
-    //IGNORE------
-    //formula(1,2,1,5);
+class file{
+public:
+    int num_of_pizzas;
+    int t_2; int t_3; int t_4;
+    std::list<string> all_pizza_ingredients; int num_of_ingredients = all_pizza_ingredients.size();
+    std::string bitmask;
+    std::vector<std::string> lines;int amount_of_lines = lines.size();
     std::vector<int> values;
-    //---------------------
+    std::list<std::string> all_ingredients;std::vector<std::string> new_ingred; std::vector<int> combos;
 
 
-    //check combos of pizzas and people
+    void removeDupWord(std::string str)
+    {
+        string word = "";
+        for (auto x : str)
+        {
+            if (x == ' ')
+            {
+                all_ingredients.push_back(word);
+                word = "";
 
-    //have equation numofpizzas <= 2T2 + 3T3 +4T4.
-    //let T2=a, T3=b,T4=c.
-    //finds all a,b,c where 2a+3b+4c <= 5 (num of pizzas) where a,b,c are INTEGERS>=0;
-    //probs a quicker way to do this, but it works
-    for (int i = 0; i<= t_2; i++){
-        for (int j = 0;j<=t_3;j++){
-            for (int z = 0;z<=t_4;z++){
-                if (2*i+3*j+4*z <= num_of_pizzas){ //adds values of a,b,c that are only less than or equal to amount of pizzas
-                    values.push_back(i);values.push_back(j);values.push_back(z);
+
+            }
+            else {
+                word = word + x;
+
+            }
+
+        }
+    }
+    void print(std::list<std::string> const &list){
+        for (auto const& i: list) {
+            std::cout << i << "\n";
+        }
+    }
+
+    void input_file(std::string path){
+        ifstream infile;
+        infile.open(path);
+        string stringFromFile;
+        if(infile.fail()){
+        cout<<"Input file failed to open";
+        }
+        else{
+            while (getline(infile,stringFromFile) ){
+              lines.push_back(stringFromFile);
+            }
+            infile.close();
+        }
+    }
+
+    //need to slightly change this to get rid of the numbers
+    void get_ingredients(){
+        int y = lines.size();
+        for(int i =1;i<y;i++){
+            lines[1]=lines[1]+" " + lines[i];
+        }
+        std::string list_all=lines[1];
+        removeDupWord(list_all);
+        all_ingredients.sort();all_ingredients.unique();
+    }
+
+    //function to find combinations. e.g. if you have N things and want to organise them in groups of K
+    //e.g 5 things organised in 3 slots
+    //comb(5,3) = 0 1 2, 0 1 3 etc
+    void comb(int N, int K)
+    {
+        std::string bitmask(K, 1); // K leading 1's
+        bitmask.resize(N, 0); // N-K trailing 0's
+        // print integers and permute bitmask
+        do {
+            for (int i = 0; i < N; ++i) // [0..N-1] integers
+            {
+                if (bitmask[i]) std::cout << " " << i;
+            }
+            std::cout << std::endl;
+        } while (std::prev_permutation(bitmask.begin(), bitmask.end()));
+    }
+
+    //this would get combos e.g. for 1T2,1T3 so 01 234, 02 134 no repeats,input would be t2,t3,t4,numofpizzas
+
+    void way_of_combinations(){
+        for (int i = 0; i<=t_2; i++){
+            for (int j = 0;j<=t_3;j++){
+                for (int z = 0;z<=t_4;z++){
+                    if (2*i+3*j+4*z <= num_of_pizzas){ //adds values of a,b,c that are only less than or equal to amount of pizzas
+                        values.push_back(i);values.push_back(j);values.push_back(z);
+                    }
                 }
             }
         }
+        int s = values.size();
+        for(int i=0;i<s;i++){
+            int d = (i+1)%3;
+            if(d==0){
+            std::cout<<values[i]<<"\n";
+            }
+            else{ std::cout<<values[i];}
+        }
     }
-    //print1(values);
+    void distribute_pizzas_to_groups(){} //for every way_of_combo, find comb for each group.
+    void calculate_num_of_ingredients_for_combinations(){} //each possible valid combo from above function, calc num of ingredients for each and square it
+    void decide_best_combination(){} //from above function, get largest one and get combo and this is the answer!!
+    void create_answer_file(){} //i think we need to make a file w answers, need to check in brief
 
-    //IGNORE-----------
-    //for every three values, assign a pizza to each person.
-    int factor = (values.size())/3;
-    //int matrix[factor][3];
-    //for(int j=0;j<factor;j++){
-    //    for (int i =0;i<3;i++){
-    //        matrix[factor][i]=values[i];
-    //    }
-    //}
-    //NEEd TO STORE EACH COMBO IN ITS OWN / MATRIX
-    //-------------------
+    };
 
+int main()
+{
+    file fileobj;
 
-    //need to take values and take every three elements and store them in a proper place
-    std::vector<int> each_combo = {values[3],values[4],values[5]};//just one example
-    int total_people = 2*each_combo[0]+3*each_combo[1]+4*each_combo[2];
+    fileobj.num_of_pizzas=5;
+    fileobj.t_2 = 1; fileobj.t_3 = 2; fileobj.t_4 = 1;
+
     //https://stackoverflow.com/questions/12991758/creating-all-possible-k-combinations-of-n-items-in-c
 
     //this bit prints out the combinations from function above
-    comb(5,2);comb(5,3);comb(5,4);
+    fileobj.comb(5,2);std::cout<<"oK:";
+    int  yx = fileobj.combos.size();
+    for(int i=0;i<yx;i++){
+        std::cout<< fileobj.combos[i];}
+    //fileobj.comb(5,3);
+    //fileobj.comb(5,4);
+
     //let's say for 2(1)+3(1)+4(0) for a,b,c.
     //need to find combs of 2,3 with no repeats, need to change comb function a lil and how its stored
     // e.g 01 234 or 02 134 NOT 12 234 as 2 is repeated
-    ifstream infile; //Container for the file
-    infile.open("/Users/apple/Downloads/a_example.txt"); //Name of the file you want to open
-    string stringFromFile; //string variables to store extracted strings
 
-    //INPUTTING FILE, this works
-    //adds each file line by line to a vector
-    std::vector<std::string> lines;
-    if(infile.fail())
-    {
-    cout<<"Input file failed to open";
-    }
-    else
-    {
-        while (getline(infile,stringFromFile) )
-        {
-          lines.push_back(stringFromFile);
-        }
-        infile.close();
-    }
-    //need to split files
+    fileobj.input_file("/Users/apple/Downloads/a_example.txt");
+    std::cout<<"lines 0: "<<fileobj.lines[0]<<"\n";
+    std::cout<<"lines 1: "<<fileobj.lines[1]<<"\n";
+    std::cout<<"lines 2: "<<fileobj.lines[2]<<"\n";
+    std::cout<<"lines 3: "<<fileobj.lines[3]<<"\n";
+    std::cout<<"lines 4: "<<fileobj.lines[4]<<"\n";
+    std::cout<<"lines 5: "<<fileobj.lines[5]<<"\n";
+
+    std::cout<<"bitmask: "<<fileobj.bitmask[0]<<"\n"; //NOT WORKING YET
+
+    fileobj.get_ingredients();
+    std::cout<<"Removing repeating ingredients: \n";
+    fileobj.print(fileobj.all_ingredients);
+
+    fileobj.way_of_combinations();
+
     return 0;
 }
